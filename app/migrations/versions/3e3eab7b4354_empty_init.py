5c0718e9d40a63c05239603e0729c6005e0062a2
IvyKibe <maleeivy@gmail.com>
2023-12-07 18:44:10 +0300

    completed lab

---
+++ app/migrations/versions/3e3eab7b4354_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 3e3eab7b4354
+Revises: 
+Create Date: 2023-12-07 17:58:54.069282
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3e3eab7b4354'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
